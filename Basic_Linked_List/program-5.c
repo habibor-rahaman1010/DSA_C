c31bfaeb96957d741a0123463fdaaaca5d7cb932
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-19 19:32:32 +0600

    Practice Linked List

--- Basic_Linked_List/program-5.c
+++ Basic_Linked_List/program-5.c
@@ -11,14 +11,33 @@ struct Node {
 };
 
 Node* Create_Node(int ara[], int length) {
+    Node* head = NULL, *new_node = NULL, *current = NULL;
 
-}
+    for(int i = 0; i < length; i++){
+        new_node = (Node *) malloc(sizeof(Node));
+        new_node->data = ara[i];
+        new_node->next = NULL;
+
+        if(head == NULL){
+            head = new_node;
+            current = new_node;
+        }else{
+            current->next = new_node;
+            current = current->next;
+        }
+    }
+    return head;
 
 int main() {
     int ara[] = {12, 23, 14, 16, 25};
     int length  = sizeof(ara) / sizeof(ara[0]);
 
     Node* head = Create_Node(ara, length);
+    while(head != NULL){
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
 
 return 0;
 }
