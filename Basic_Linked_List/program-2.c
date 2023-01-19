e200c4f071f5687d62ec120e792c3b68ed6f81e1
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-19 10:29:22 +0600

    Learn Linked List

--- Basic_Linked_List/program-2.c
+++ Basic_Linked_List/program-2.c
@@ -1,15 +1,43 @@
 //implement linked list in c program...
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int CreateLinkedList(int ara, int size){
+typedef struct Node Node;
+struct Node{
+    int data;
+    Node* next;
+};
 
+Node* CreateLinkedList(int ara[], int size){
+    Node* head = NULL, *temp = NULL, *current = NULL;
+
+    for(int i = 0; i < size; i++){
+        temp = (Node *) malloc(sizeof(Node));
+        temp->data = ara[i];
+        temp->next = NULL;
+
+        if(head == NULL){
+           head = temp;
+           current = temp;
+        }else{
+            current->next = temp;
+            current = current->next;
+        }
+    }
+    return head;
 }
 
 int main() {
     int ara[] = {12, 14, 16, 20, 25};
     int size = sizeof(ara) / sizeof(ara[0]);
-    CreateLinkedList(ara, size);
+    Node* head = CreateLinkedList(ara, size);
+
+    while(head != NULL){
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL \n");
 
 return 0;
 }
