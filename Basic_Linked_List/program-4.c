a82230f681814c3e3acaf54e86fbd6dbdb6de88f
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-19 13:18:41 +0600

    Hello programmer

--- Basic_Linked_List/program-4.c
+++ Basic_Linked_List/program-4.c
@@ -11,7 +11,37 @@ struct Node{
 };
 
 Node* Create_Node(int ara[], int length) {
+    Node* head = NULL, *temp = NULL, *current = NULL;
 
+    for(int i = 0; i < length; i++){
+        temp = (Node *) malloc(sizeof(Node));
+        temp->data = ara[i];
+        temp->next = NULL;
+
+        if(head == NULL){
+            head = temp;
+            current = temp;
+        }
+        else{
+            current->next = temp;
+            current = current->next;
+        }
+    }
+    return head;
+}
+
+Node* Print_Reverse(Node* head) {
+    Node* prv = NULL;
+    Node* current = head;
+    Node* next = head->next;
+
+    while(current != NULL) {
+        prv = current->next;
+        current->next = prv;
+        prv = current;
+        current = next;
+    }
+    return prv;
 }
 
 int main() {
@@ -19,6 +49,20 @@ int main() {
     int length = sizeof(ara) / sizeof(ara[0]);
 
     Node* head = Create_Node(ara, length);
+
+    Node* a = head;
+    while(a != NULL){
+        printf("%d ", a->data);
+        a = a->next;
+    }
+    printf("\n");
+
+    Node* b = Print_Reverse(head);
+    while(b != NULL){
+        printf("%d ", b->data);
+        b = b->next;
+    }
+    printf("\n");
 
 return 0;
 }
