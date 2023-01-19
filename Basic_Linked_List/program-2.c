7979d577346723b451c5112889a459e72019b27f
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-19 09:38:22 +0600

    Learn Linked List

--- Basic_Linked_List/program-2.c
+++ Basic_Linked_List/program-2.c
@@ -1,1 +1,15 @@
 //implement linked list in c program...
+
+#include <stdio.h>
+
+int CreateLinkedList(int ara, int size){
+
+}
+
+int main() {
+    int ara[] = {12, 14, 16, 20, 25};
+    int size = sizeof(ara) / sizeof(ara[0]);
+    CreateLinkedList(ara, size);
+
+return 0;
+}
