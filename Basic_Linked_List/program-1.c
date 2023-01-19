9f6221f5549b04a167cfec0eee1cfadc243bfa3b
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-19 08:17:18 +0600

    Learn Linked List

--- Basic_Linked_List/program-1.c
+++ Basic_Linked_List/program-1.c
@@ -11,7 +11,7 @@ struct Node {
 };
 
 int main() {
-    Node* a = NULL, *b = NULL, *c = NULL, *d = NULL, *e = NULL;
+    Node* a = NULL, *b = NULL, *c = NULL, *d = NULL, *e = NULL, *f = NULL;
 
     //dynamic memory allocate...
     a =  (Node *) malloc(sizeof(Node));
@@ -19,6 +19,7 @@ int main() {
     c =  (Node *) malloc(sizeof(Node));
     d =  (Node *) malloc(sizeof(Node));
     e =  (Node *) malloc(sizeof(Node));
+    f =  (Node *) malloc(sizeof(Node));
 
     //value assign in node...
     a->data = 10;
@@ -26,18 +27,29 @@ int main() {
     c->data = 30;
     d->data = 40;
     e->data = 50;
+    f->data = 60;
 
     a->next = b;
     b->next = c;
     c->next = d;
     d->next = e;
-    e->next = NULL;
+    e->next = f;
+    f->next = NULL;
 
     Node* head = a;
     while(head != NULL){
         printf("%d ", head->data);
         head = head->next;
     }
+    printf("\n");
+
+    free(a);
+    free(b);
+    free(c);
+    free(d);
+    free(e);
+    free(f);
+    free(head);
 
 return 0;
 }
