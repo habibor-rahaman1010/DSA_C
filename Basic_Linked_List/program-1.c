2bdf1f2781c5a83317791a72f92dd247fe1d026e
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-19 08:10:38 +0600

    Learn Linked List

--- Basic_Linked_List/program-1.c
+++ Basic_Linked_List/program-1.c
@@ -1,9 +1,43 @@
 //Linked List in c program...
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node Node;
+
+struct Node {
+    int data;
+    Node* next;
+};
 
 int main() {
+    Node* a = NULL, *b = NULL, *c = NULL, *d = NULL, *e = NULL;
 
+    //dynamic memory allocate...
+    a =  (Node *) malloc(sizeof(Node));
+    b =  (Node *) malloc(sizeof(Node));
+    c =  (Node *) malloc(sizeof(Node));
+    d =  (Node *) malloc(sizeof(Node));
+    e =  (Node *) malloc(sizeof(Node));
+
+    //value assign in node...
+    a->data = 10;
+    b->data = 20;
+    c->data = 30;
+    d->data = 40;
+    e->data = 50;
+
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+    e->next = NULL;
+
+    Node* head = a;
+    while(head != NULL){
+        printf("%d ", head->data);
+        head = head->next;
+    }
 
 return 0;
 }
